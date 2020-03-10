d56953ce32bcaf4837e72e5bd2f7c5a1aead0255
xunto <hunterdelyx1@gmail.com>
2020-03-10 20:54:41 +0300

    Enforce specific classloader to access resources

--- roleplay_chat_dices-common/src/main/java/ru/xunto/roleplaychat/dices/RollCommand.java
+++ roleplay_chat_dices-common/src/main/java/ru/xunto/roleplaychat/dices/RollCommand.java
@@ -1,6 +1,10 @@
 package ru.xunto.roleplaychat.dices;
 
 import org.jparsec.error.ParserException;
+import org.jtwig.environment.EnvironmentConfiguration;
+import org.jtwig.resource.loader.ClasspathResourceLoader;
+import org.jtwig.resource.loader.TypedResourceLoader;
+import org.jtwig.resource.reference.ResourceReference;
 import ru.xunto.roleplaychat.RoleplayChatCore;
 import ru.xunto.roleplaychat.api.ICommand;
 import ru.xunto.roleplaychat.api.ISpeaker;
@@ -19,20 +23,32 @@ import ru.xunto.roleplaychat.framework.renderer.text.TextColor;
 import ru.xunto.roleplaychat.framework.state.IProperty;
 import ru.xunto.roleplaychat.framework.state.Property;
 
+import java.util.Collections;
 import java.util.List;
 
 public class RollCommand implements ICommand {
     public static IProperty<IRoll> roll = new Property<>("roll");
     public static IProperty<List<TextPart>> result = new Property<>("result");
     public static IProperty<Integer> finalResult = new Property<>("final_result");
-    private static JTwigTemplate template = new JTwigTemplate("assets/roleplaychatdices/templates/dices.twig");
+    private static EnvironmentConfiguration CONF = JTwigTemplate.CONF_BUILDER
+            .resources()
+            .resourceLoaders().set(
+                    Collections.singletonList(new TypedResourceLoader(
+                            ResourceReference.CLASSPATH,
+                            new ClasspathResourceLoader(RollCommand.class.getClassLoader())
+                    ))
+            ).and()
+            .and()
+            .build();
+
+
+    private static JTwigTemplate template = new JTwigTemplate("assets/roleplaychatdices/templates/dices.twig", CONF);
 
     public void sendDiceResult(ISpeaker speaker, IRoll roll, IResult result) {
         Request request = new Request("", speaker);
         Environment environment = new Environment(speaker.getName(), "");
         JTwigState state = environment.getState();
         environment.setProcessed(true);
-
         environment.setTemplate(template);
         environment.getColors().put("default", TextColor.GOLD);
         environment.getColors().put("critical_success", TextColor.GREEN);
