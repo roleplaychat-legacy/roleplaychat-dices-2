31268f620df36f7e58c6174c844046d84de74b19
star191 <star191@keemail.me>
2020-03-21 14:40:16 +0500

    mod meta refactoring

--- roleplay_chat_dices-forge-1.12.2/src/main/java/ru/xunto/roleplaychat/dices/forge_1_12_2/RoleplayChatDices.java
+++ roleplay_chat_dices-forge-1.12.2/src/main/java/ru/xunto/roleplaychat/dices/forge_1_12_2/RoleplayChatDices.java
@@ -7,12 +7,11 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = RoleplayChatDices.MODID, name = RoleplayChatDices.NAME, version = RoleplayChatDices.VERSION)
+@Mod(modid = RoleplayChatDices.MODID, version = RoleplayChatDices.VERSION, acceptableRemoteVersions = "*", dependencies = "required-after:roleplaychat")
 public class RoleplayChatDices
 {
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
-    public static final String VERSION = "1.0";
+    public static final String MODID = "@@MODID@@";
+    public static final String VERSION = "@@VERSION@@";
 
     private static Logger logger;
 
