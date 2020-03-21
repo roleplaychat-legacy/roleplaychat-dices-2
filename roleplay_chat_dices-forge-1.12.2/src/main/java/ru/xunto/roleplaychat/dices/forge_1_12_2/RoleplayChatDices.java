56c19219d8f8e1c5c9c881c3a3d5eecbd106e0eb
star191 <star191@keemail.me>
2020-03-21 18:08:28 +0500

    merge update with master

--- roleplay_chat_dices-forge-1.12.2/src/main/java/ru/xunto/roleplaychat/dices/forge_1_12_2/RoleplayChatDices.java
+++ roleplay_chat_dices-forge-1.12.2/src/main/java/ru/xunto/roleplaychat/dices/forge_1_12_2/RoleplayChatDices.java
@@ -1,34 +1,14 @@
 package ru.xunto.roleplaychat.dices.forge_1_12_2;
 
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import org.apache.logging.log4j.Logger;
-import ru.xunto.roleplaychat.RoleplayChatCore;
-import ru.xunto.roleplaychat.dices.RollCommand;
+import ru.xunto.roleplaychat.dices.RoleplayChatDicesCore;
 
 @Mod(modid = RoleplayChatDices.MODID, version = RoleplayChatDices.VERSION, acceptableRemoteVersions = "*", dependencies = "required-after:roleplaychat")
-public class RoleplayChatDices
-{
+public class RoleplayChatDices {
     public static final String MODID = "@@MODID@@";
     public static final String VERSION = "@@VERSION@@";
 
-    private static Logger logger;
-
     static {
-        RoleplayChatCore.instance.register(new RollCommand());
-    }
-
-    @EventHandler
-    public void preInit(FMLPreInitializationEvent event)
-    {
-        logger = event.getModLog();
-    }
-
-    @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-        logger.info("init");
+        RoleplayChatDicesCore.init();
     }
 }
